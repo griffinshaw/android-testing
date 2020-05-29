8daabce398ed87d4ad02e278c1936b5efe8bf5ef
Griffin Shaw <gshaw.development@gmail.com>
2020-05-28 21:41:56 -0500

    Laid out add new task test

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,5 +1,18 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import org.junit.Assert.*
+import org.junit.Test
 
-class TasksViewModelTest
+class TasksViewModelTest {
+
+    @Test
+    fun addNewTask_setNewTaskEvent() {
+        // TODO: Given a fresh TasksViewModel
+
+
+        // TODO: When adding a new task
+
+
+        // TODO: Then the new task event is triggered
+    }
+}
