0e13f42aec8d341d049acfb0b84857acdf38a9fd
Griffin Shaw <gshaw.development@gmail.com>
2020-06-07 13:03:56 -0700

    Created test for task detail fragment

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -0,0 +1,17 @@
+package com.example.android.architecture.blueprints.todoapp.taskdetail
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class TaskDetailFragmentTest {
+
+    @Before
+    fun setUp() {
+    }
+}
