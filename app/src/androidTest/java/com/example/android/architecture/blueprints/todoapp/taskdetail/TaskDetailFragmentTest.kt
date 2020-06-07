3a1f467ccb6d4b48e2476d63710f718772ad28bd
Griffin Shaw <gshaw.development@gmail.com>
2020-06-07 13:11:08 -0700

    Started test to display task in fragment

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -1,10 +1,14 @@
 package com.example.android.architecture.blueprints.todoapp.taskdetail
 
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @MediumTest
@@ -13,5 +17,16 @@ class TaskDetailFragmentTest {
 
     @Before
     fun setUp() {
+    }
+
+    @Test
+    fun activeTaskDetails_DisplayedInUi() {
+        // GIVEN - Add active (incomplete) task to the DB
+        val activeTask = Task("Active Task", "AndroidX Rocks", false)
+
+        // WHEN - Details fragment launched to display task
+        val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+
     }
 }
