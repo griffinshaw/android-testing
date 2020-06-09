3bd4dcf36b17f4c9262869c1932d5cd6c9b12ed6
Griffin Shaw <gshaw.development@gmail.com>
2020-06-08 17:58:18 -0700

    Completed navigation test

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
@@ -2,6 +2,13 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
@@ -16,6 +23,8 @@ import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
@@ -23,6 +32,7 @@ import org.junit.runner.RunWith
 class TasksFragmentTest {
 
     private lateinit var repository: TasksRepository
+
 
     @Before
     fun initRepository() {
@@ -37,6 +47,21 @@ class TasksFragmentTest {
 
         // GIVEN - On the home screen
         val scenario = launchFragmentInContainer<TasksFragment>(Bundle(), R.style.AppTheme)
+
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        // WHEN - Click on the first list item
+        onView(withId(R.id.tasks_list))
+                .perform(RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                        hasDescendant(withText("TITLE1")), click()))
+
+        // THEN - Verify that we navigate to the first detail screen
+        verify(navController).navigate(
+                TasksFragmentDirections.actionTasksFragmentToTaskDetailFragment( "id1")
+        )
 
     }
 
